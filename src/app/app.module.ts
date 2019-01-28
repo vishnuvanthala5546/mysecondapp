9010087f2abc826e2864e7acd6ad7608662a9afe
Vishnupatel <vcvanthala.5546@gmail.com>
2019-01-28 19:25:24 +0530

    151 Styling Active Recipe Items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { DropdownDirective } from './shared/dropdown.directive';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { FormdataComponent } from './formdata/formdata.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent
+    RecipeStartComponent,
+    FormdataComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/formdata/formdata.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-formdata',
+  templateUrl: './formdata.component.html',
+  styleUrls: ['./formdata.component.scss']
+})
+export class FormdataComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
